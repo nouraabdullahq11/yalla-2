71537ba7960fbb9d6a24c2c317c2aef5204fdeed
noura <nouraabdullahahq@gmail.com>
2023-10-08 18:07:55 +0300

    Initial Commit

---
+++ yalla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yalla
+//
+//  Created by Noura Alqahtani on 08/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yalla/yallaApp.swift
@@ -0,0 +1,17 @@
+//
+//  yallaApp.swift
+//  yalla
+//
+//  Created by Noura Alqahtani on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct yallaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
